dbb9f55c69f277c9975b0e035a90c0029b517b9c
Ninibeth Teixeira <ninibethreina@gmail.com>
2020-04-01 14:04:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,10 +2,15 @@ katz_deli=[]
 
 def line(katz_deli) 
   if katz_deli= []
-    num=1 
+     
     puts "The line is currently empty."
     
   else 
-    puts "The line is currently: #{num}. #{name}"
+    
+    string= "The line is currently:"
+    katz_deli.each_with_index(1) do |person, index|
+      string += "#{index}. #{person}"
+    end 
+  puts string 
   end 
 end 
